2bbe6e8d1c4db9740c188ad5c2738a3c6cd99820
frontDeveloper <gurbansahadowkemal01@gmail.com>
2025-02-08 11:56:02 +0300

    знакомство с actionCreators

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import { bindActionCreators, createStore } from 'redux';
+import * as actions from './actions';
+import reducer from './reducer';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const store = createStore(reducer);
+const { dispatch, subscribe, getState } = store;
 
+const update = () => {
+  document.getElementById('counter').textContent = getState().value;
+};
+
+subscribe(update);
+
+const { dec, inc, rnd } = bindActionCreators(actions, dispatch);
+
+document.getElementById('dec').addEventListener('click', dec);
+
+document.getElementById('inc').addEventListener('click', inc);
+
+document.getElementById('rnd').addEventListener('click', function () {
+  const value = Math.floor(Math.random() * 10);
+  rnd(value);
+});
+
+createRoot(document.getElementById('root')).render(<StrictMode></StrictMode>);
